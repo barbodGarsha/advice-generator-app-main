94c379215b2632dac2b572ff5bbc7fa532bc8346
Barbod Garsha <barbodgarshasbi79@gmail.com>
2023-02-01 16:33:45 +0100

    finished the project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+advice_id = document.querySelector('[data-advice-id]')
+advice_qoute = document.querySelector('[data-advice-qoute]')
+get_advice_btn = document.querySelector('[data-get-advice-btn]')
+
+
+
+get_advice_btn.addEventListener('click', function(e) {
+    get_advice()
+
+    // disable the button for 2s because the Advice is cached for 2 seconds. 
+    // Any repeat-request within 2 seconds will return the same piece of advice.
+    get_advice_btn.style.setProperty('pointer-events', 'none')
+    setTimeout(function() {
+        get_advice_btn.style.setProperty('pointer-events', 'unset')
+    }, 2000)
+})
+
 //API----------------------------------------------------------
 const adviceslip_api_url = 'https://api.adviceslip.com/advice'
 
@@ -5,7 +22,7 @@ async function get_advice() {
   const response = await fetch(adviceslip_api_url)
   const data = await response.json()
 
-  console.log(data)
-  console.log(data['slip']['id'])
-  console.log(data['slip']['advice'])
+  
+  advice_id.innerHTML = data['slip']['id'];
+  advice_qoute.innerHTML = data['slip']['advice'];
 }
